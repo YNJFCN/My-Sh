f127b3d3b7879590e47189025f86aca77acdaeb0
YNJFCN <ynjfcn@gmail.com>
2024-02-26 21:52:01 +0800

    delete default

--- ac.sh
+++ ac.sh
@@ -33,6 +33,52 @@ confirm() {
     fi
 }
 
+release(){
+        ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+        if [ $? -ne 0 ]; then
+            LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
+            exit 1
+        fi
+
+    ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
+        if [ $? -ne 0 ]; then
+            LOGE "证书签发失败,脚本退出"
+            exit 1
+        else
+            LOGI "证书签发成功,安装中..."
+        fi
+
+    certPath="/root/Certificate/${CF_Domain}"
+    if [ ! -d "$certPath" ]; then
+        mkdir "$certPath"
+    else
+        rm -rf "$certPath"
+        mkdir "$certPath"
+    fi
+    ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file ${certPath}/ca.cer \
+    --cert-file ${certPath}/${CF_Domain}.cer --key-file ${certPath}/${CF_Domain}.key \
+    --fullchain-file ${certPath}/full.${CF_Domain}.cer
+        if [ $? -ne 0 ]; then
+            LOGE "证书安装失败,脚本退出"
+            exit 1
+        else
+            LOGI "安装路径为${certPath}目录"
+            LOGI "证书安装成功,开启自动更新..."
+        fi
+
+    ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+        if [ $? -ne 0 ]; then
+            LOGE "自动更新设置失败,脚本退出"
+            ls -lah cert
+            chmod 755 $certPath
+            exit 1
+        else
+            LOGI "证书已安装且已开启自动更新,具体信息如下"
+            ls -lah cert
+            chmod 755 $certPath
+        fi
+}
+
 echo -E ""
 LOGD "******使用说明******"
 LOGI "该脚本将使用Acme脚本申请证书,使用时需保证:"
@@ -51,70 +97,23 @@ if [ $? -eq 0 ]; then
         LOGE "安装acme脚本失败"
         exit 1
     fi
-
     CF_Domain=""
     CF_GlobalKey=""
     CF_AccountEmail=""
-    certPath=/root/Certificate
-    
+    certPath=/root/Certificate    
     if [ ! -d "$certPath" ]; then
         mkdir $certPath
     else
         rm -rf $certPath
         mkdir $certPath
     fi
-
     LOGD "是否直接颁发证书"
     read -p "[y/n]" DONT
     if [ "$DONT" = "y" ] || [ "$DONT" = "Y" ];then
-
     LOGD "请设置要申请的域名:"
     read -p "Input your domain here:" CF_Domain
-    LOGD "你的域名设置为:${CF_Domain}"
-
-    ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-        if [ $? -ne 0 ]; then
-            LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
-            exit 1
-        fi
-
-    ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
-        if [ $? -ne 0 ]; then
-            LOGE "证书签发失败,脚本退出"
-            exit 1
-        else
-            LOGI "证书签发成功,安装中..."
-        fi
-
-    certPath="/root/Certificate/${CF_Domain}"
-    if [ ! -d "$certPath" ]; then
-        mkdir "$certPath"
-    else
-        rm -rf "$certPath"
-        mkdir "$certPath"
-    fi
-    ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file ${certPath}/ca.cer \
-    --cert-file ${certPath}/${CF_Domain}.cer --key-file ${certPath}/${CF_Domain}.key \
-    --fullchain-file ${certPath}/full.${CF_Domain}.cer
-        if [ $? -ne 0 ]; then
-            LOGE "证书安装失败,脚本退出"
-            exit 1
-        else
-            LOGI "安装路径为${certPath}目录"
-            LOGI "证书安装成功,开启自动更新..."
-        fi
-
-    ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-        if [ $? -ne 0 ]; then
-            LOGE "自动更新设置失败,脚本退出"
-            ls -lah cert
-            chmod 755 $certPath
-            exit 1
-        else
-            LOGI "证书已安装且已开启自动更新,具体信息如下"
-            ls -lah cert
-            chmod 755 $certPath
-        fi
+    LOGD "你的域名设置为:${CF_Domain}"  
+    release
 
     else
         LOGD "请设置域名:"
@@ -126,52 +125,13 @@ if [ $? -eq 0 ]; then
         LOGD "请设置注册邮箱:"
         read -p "Input your email here:" CF_AccountEmail
         LOGD "你的注册邮箱为:${CF_AccountEmail}"
-
     ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
         if [ $? -ne 0 ]; then
             LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
             exit 1
         fi
-
         export CF_Key="${CF_GlobalKey}"
         export CF_Email=${CF_AccountEmail}
-
-    ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
-        if [ $? -ne 0 ]; then
-            LOGE "证书签发失败,脚本退出"
-            exit 1
-        else
-            LOGI "证书签发成功,安装中..."
-        fi
-
-    certPath="/root/Certificate/${CF_Domain}"
-    if [ ! -d "$certPath" ]; then
-        mkdir "$certPath"
-    else
-        rm -rf "$certPath"
-        mkdir "$certPath"
-    fi
-    ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file ${certPath}/ca.cer \
-    --cert-file ${certPath}/${CF_Domain}.cer --key-file ${certPath}/${CF_Domain}.key \
-    --fullchain-file ${certPath}/full.${CF_Domain}.cer
-        if [ $? -ne 0 ]; then
-            LOGE "证书安装失败,脚本退出"
-            exit 1
-        else
-            LOGI "安装路径为${certPath}目录"
-            LOGI "证书安装成功,开启自动更新..."
-        fi
-
-    ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-        if [ $? -ne 0 ]; then
-            LOGE "自动更新设置失败,脚本退出"
-            ls -lah cert
-            chmod 755 $certPath
-            exit 1
-        else
-            LOGI "证书已安装且已开启自动更新,具体信息如下"
-            ls -lah cert
-            chmod 755 $certPath
-        fi
+        release
     fi
 fi
